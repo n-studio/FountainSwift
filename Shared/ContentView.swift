cdde0190bc2ad264e267988f69d27f41dd7943b5
Matthew Nguyen <contact@n-studio.fr>
2021-01-11 00:24:49 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Matthew Nguyen on 11/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FountainSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FountainSwiftApp.swift
+//  Shared
+//
+//  Created by Matthew Nguyen on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct FountainSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
